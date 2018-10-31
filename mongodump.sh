8b182a37a385b68c7e24218e3f3c5dd23389741a
kk <post@koljakutschera.de>
2018-10-31 21:37:38 +0100

    mongodump done

--- mongodump.sh
+++ mongodump.sh
@@ -5,8 +5,6 @@ trap "exit" INT
 dumpMongo () {
   echo 'mongodump running...'
   NOW=$(date +"%Y-%m-%d_%H-%M")
-  echo $MONGO_INITDB_ROOT_USERNAME
-  echo $MONGO_INITDB_ROOT_PASSWORD
   mongodump --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "admin" --verbose --host "mongo" --port 27017 --gzip --archive="/mongodump/${NOW}_mongodump.archive"
   echo 'mongodump cleaning...'
   for file in $(find /mongodump/*_mongodump* -maxdepth 0 -mtime +7)
