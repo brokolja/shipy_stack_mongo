4f2df6049936079bb411e62a7a44cead0ad6b4be
kk <post@koljakutschera.de>
2018-10-31 21:13:01 +0100

    add mongodump

---
+++ mongodump.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+trap "exit" INT
+
+dumpMongo () {
+  echo 'mongodump running...'
+  NOW=$(date +"%Y-%m-%d_%H-%M")
+  mongodump --username "$MONGO_INITDB_ROOT_USERNAME" --password "$MONGO_INITDB_ROOT_PASSWORD" --authenticationDatabase "admin" --verbose --host "mongo" --port 27017 --gzip --archive="/mongodbdump/${NOW}_mongodump.archive"
+  echo 'mongodump cleaning...'
+  for file in $(find /mongodbdump/*_mongodump* -maxdepth 0 -mtime +7)
+  do
+      echo "mongodump removing old backup: $file"
+      rm -rf $file
+  done
+}
+
+mkdir -p /mongodump
+
+while true; do
+  dumpMongo
+  sleep 1d
+done
